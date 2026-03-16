5c6f3e77906710be717e8611e9a32d1e6e50435a
misakait <cjl2597588144@gmail.com>
2026-03-16 23:56:55 +0800

    refactor： TalentCard新增材质字段

--- resources/talents/TalentData.cs
+++ resources/talents/TalentData.cs
@@ -7,4 +7,5 @@ public partial class TalentData : Resource
 {
     [Export] public string TalentName { get; set; }
     [Export(PropertyHint.MultilineText)] public string Description { get; set; }
+    [Export] public Texture2D TalentTexture { get; set; }
 }
