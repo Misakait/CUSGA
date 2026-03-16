09bbae44ac56dae1c520b02a5e3bab6922834ba8
misakait <cjl2597588144@gmail.com>
2026-03-16 21:44:26 +0800

    feat: 新增talent resource

---
+++ resources/talents/TalentData.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace CUSGA.resources.talents;
+
+[GlobalClass]
+public partial class TalentData : Resource
+{
+    [Export] public string TalentName { get; set; }
+    [Export(PropertyHint.MultilineText)] public string Description { get; set; }
+}
