7a92abd35c59ebc4975cf22fa4ddc5489efbb785
LIsBam <2310773349@qq.com>
2026-03-17 22:12:45 +0800

    feat:新增手牌功能。实现手牌的自动排序

--- scripts/card_scripts/card_manager.gd
+++ scripts/card_scripts/card_manager.gd
@@ -6,10 +6,12 @@ const COLLISION_MASK_CARD_SLOT = 2
 var screen_size
 var card_being_dragged:Node2D
 var is_hovering_on_card:bool
+var player_hand_referencd #玩家手牌引用
 var drag_offset: Vector2 # 用于记录拖拽偏移量
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	player_hand_referencd = $"../PlayerHand"
 
 func _process(delta: float) -> void:
 	if card_being_dragged:
@@ -41,10 +43,15 @@ func finish_drag():
 	card_being_dragged.scale = Vector2(1.05, 1.05)
 	var card_slot_found = raycast_check_for_card_slot()
 	if  card_slot_found and not card_slot_found.card_in_slot:
+		player_hand_referencd.remove_card_from_hand(card_being_dragged)
 		card_being_dragged.position = card_slot_found.position
 		#若不想让卡牌嵌入卡槽后不可移动，注释掉下两行代码
 		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
 		card_slot_found.card_in_slot = true
+	else:#如果拖动后没进入卡槽，则回到玩家手中
+		player_hand_referencd.add_card_to_hand(card_being_dragged)
+		
+		
 	card_being_dragged = null
 
 func connect_card_signals(card):
