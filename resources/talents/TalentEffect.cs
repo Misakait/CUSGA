90e38c2b1e6d84bcd83eb72f95052227a3fbd8de
misakait <cjl2597588144@gmail.com>
2026-03-18 19:34:12 +0800

    feat：定义天赋效果基类

---
+++ resources/talents/TalentEffect.cs
@@ -0,0 +1,9 @@
+using Godot;
+using CUSGA.entities;
+namespace CUSGA.resources.talents;
+
+[GlobalClass]
+public abstract partial class TalentEffect : Resource
+{
+    public abstract void Apply(Player targetPlayer);
+}
