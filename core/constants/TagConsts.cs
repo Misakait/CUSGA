64b025f21cb544895f60842bba5128cc9d729f86
misakait <cjl2597588144@gmail.com>
2026-03-18 19:43:53 +0800

    feat： 引入标签天赋资源和标签天赋字符串常量

---
+++ core/constants/TagConsts.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace CUSGA.core.constants;
+
+public static class TagConsts
+{
+    // 以下是示例词条
+    public static readonly StringName BonusWoodDrop = new("BonusWoodDrop");
+    public static readonly StringName PoisonImmunity = new("PoisonImmunity");
+    public static readonly StringName NightRage = new("NightRage");
+}
---
+++ resources/talents/TagTalentEffect.cs
@@ -0,0 +1,18 @@
+using Godot;
+using CUSGA.entities;
+namespace CUSGA.resources.talents;
+
+[GlobalClass]
+public partial class TagTalentEffect : TalentEffect
+{
+    [Export] public StringName TagToGrant { get; set; }
+
+    public override void Apply(Player targetPlayer)
+    {
+        if (TagToGrant != null && !TagToGrant.IsEmpty)
+        {
+            targetPlayer.ActiveTalentTags.Add(TagToGrant);
+            GD.Print($"玩家获得了特殊机制词条：{TagToGrant}");
+        }
+    }
+}
