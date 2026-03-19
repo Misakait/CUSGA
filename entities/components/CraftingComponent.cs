c5ba2a97dbc6858bf9057e217e5a8ac2eca23f8a
huhu9 <huhu998s@outlook.com>
2026-03-19 23:20:13 +0800

    Revert "fix：完全修复地图数量缺少以及小地图显示的错误的bug"
    This reverts commit 7f07887e62b9a3d63f36c73e16635a2fa2496f28.

---
+++ entities/components/CraftingComponent.cs
@@ -0,0 +1,50 @@
+using Godot;
+using CUSGA.resources.crafting;
+
+namespace CUSGA.components;
+
+public partial class CraftingComponent : Node
+{
+    private InventoryComponent _inventory;
+
+    public override void _Ready()
+    {
+        _inventory = GetParent().GetNode<InventoryComponent>("InventoryComponent");
+    }
+
+    public bool CanCraft(CraftingRecipe recipe)
+    {
+        if (recipe == null || recipe.Inputs.Count == 0) return false;
+
+        foreach (var ingredient in recipe.Inputs)
+        {
+            if (!_inventory.HasItem(ingredient.RequiredItem, ingredient.Amount))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public bool TryCraft(CraftingRecipe recipe)
+    {
+        if (!CanCraft(recipe))
+        {
+            GD.Print("材料不足，合成失败！");
+            return false;
+        }
+
+        // 扣除所有输入材料
+        foreach (var ingredient in recipe.Inputs)
+        {
+            _inventory.RemoveItem(ingredient.RequiredItem, ingredient.Amount);
+        }
+
+        // 发放输出物品
+        _inventory.AddItem(recipe.OutputItem, recipe.OutputAmount);
+
+        GD.Print($"成功合成了 {recipe.OutputAmount} 个 {recipe.OutputItem.ItemName}！");
+        return true;
+    }
+}
--- scripts/map_scripts/map_position_create.gd
+++ scripts/map_scripts/map_position_create.gd
@@ -5,217 +5,162 @@ extends Node2D
 @export var map_len_x: int
 @export var map_len_y: int
 
+var count_count: int = 0
+
 var max_dis_from_home: int = 0
-var max_dis_from_home_point: Array = [1,1]
+var max_dis_from_home_point: Vector2i = Vector2i(1, 1)
 
-#记录房间之间的链接{vector2i : [1,1,1,1]}
-var scene_to_scene: Dictionary
+# 房间之间的链接 {Vector2i : [上,右,下,左]}
+var scene_to_scene: Dictionary = {}
 
-#创建地图的顺序
-var map_arr: Array
-#搜索顺序
-var map_search_arr: Array
-var map_point_has_search: Dictionary
+# 创建地图的顺序
+var map_arr: Array[Vector2i] = []
+# 搜索顺序
+var map_search_arr: Array[Vector2i] = []
+var map_point_has_search: Dictionary = {}
 
-var map: Array
+var map: Array = []
 
 func _ready() -> void:
-	
 	randomize()
+	print("实际剩下", map_types_ref.get_map_count_now())
 	
-	#初始化
-	for y: int in range(map_len_y + 1):
-		var row: Array = []
-		for x: int in range(map_len_x + 1):
-			row.append("void")
-		map.append(row)
-	map[1][1] = "home"
+	# 初始化二维数组
+	for x in range(map_len_x + 1):
+		var col: Array = []
+		for y in range(map_len_y + 1):
+			col.append("void")
+		map.append(col)
 	
 	if map_types_ref.get_map_count_now() > 0:
 		scene_to_scene[Vector2i(1,1)] = [0,0,0,0]
-		bfs_create_map(max_dis_from_home_point)
+		bfs_create_map(Vector2i(1,1))
+	print("实际剩下", map_types_ref.get_map_count_now())
 	
-	map_search_arr = [[1,1]]
-	bfs_search([1,1])
-	map[max_dis_from_home_point[0]][max_dis_from_home_point[1]] = "boss_room"
+	map_search_arr = [Vector2i(1,1)]
+	bfs_search()
 	
-	#显示地图
-	for index_x in map.size():
-		print(map[index_x])
+	print(map[max_dis_from_home_point.x][max_dis_from_home_point.y])
+	map[max_dis_from_home_point.x][max_dis_from_home_point.y] = "boss_room"
+	map[1][1] = "home"
+	
+	# 显示地图
+	for col in map:
+		print(col)
 
-#构建二维数组地图
-func bfs_create_map(start_point: Array):
+# 构建二维数组地图
+func bfs_create_map(start_point: Vector2i):
+	if map[start_point.x][start_point.y] == "void":
+		map[start_point.x][start_point.y] = create_what()
+		print("第", count_count, "次:")
+		choose_create_position(start_point)
+		print(start_point, " ", map[start_point.x][start_point.y])
+		print("实际剩下： ",map_types_ref.get_map_count_now(), "map_arr: ", map_arr.size())
 	
-	map[start_point[0]][start_point[1]] = create_what()
-	
-	choose_create_position(start_point)
 	
 	if !map_arr.is_empty():
-		var next_point: Array = map_arr.pop_front()
+		var next_point: Vector2i = map_arr.pop_front()
 		bfs_create_map(next_point)
 
-
-func choose_create_position(start_point: Array):
-	
-	var new_start_point: Vector2i = Vector2i(start_point[0],start_point[1])
-	
-	#这个场景可衍生的场景
+func choose_create_position(start_point: Vector2i):
 	var scene_can_go: Array = [0,0,0,0]
-	#这个场景最多可以衍生的场景数,并记录可衍生的场景
-	var cnt: int = 4
-	var cnt_id = 0
-	for num in scene_to_scene[Vector2i(start_point[0],start_point[1])]:
-		cnt -= num
-		if num == 0:
-			scene_can_go[cnt_id] = 1
-		cnt_id += 1
-		
+	var cnt: int = 0
 	
-	#与剩余可生成场景比较,防止cnt大于最大可生成场景数
-	if cnt > map_types_ref.map_type_total_cnt:
-		cnt = map_types_ref.map_type_total_cnt
+	# 检查四个方向能否生成
+	var directions = [
+		Vector2i(-1,0), 
+		Vector2i(0,1),  
+		Vector2i(1,0),  
+		Vector2i(0,-1) 
+	]
+	
+	for i in range(4):
+		var new_pos = start_point + directions[i]
+		if if_create(new_pos) && map_types_ref.get_map_count_now() - cnt> map_arr.size():
+			scene_can_go[i] = 1
+			cnt += 1
 		
-	if cnt <= 0:
+	count_count += 1
+	
+	if cnt == 0 || map_types_ref.map_type_total_cnt == 0:
 		return
-		
-	#这个场景衍生个多少场景
-	var num = randi_range(1,cnt)
-	map_types_ref.map_type_total_cnt -= num
-	print(start_point," ",num)
-	print(map_types_ref.map_type_total_cnt)
 	
-	for scene_id in range(0,num):
-		var choose_position: int = 0
-		var choose_position_id = randi_range(1,cnt)
+	# 限制生成数量，至少 1 个，最多 cnt
+	var num = randi_range(1, min(cnt, map_types_ref.map_type_total_cnt))
+	map_types_ref.map_type_total_cnt -= num
+	
+	print("num:", num, " ", scene_can_go, " cnt=", cnt, "剩下",  map_types_ref.map_type_total_cnt, " 实际剩下", map_types_ref.get_map_count_now())
+	
+	# 强制保证每个新房间和父房间双向连接
+	for i in range(num):
+		var available_dirs: Array = []
+		for j in range(4):
+			if scene_can_go[j] == 1:
+				available_dirs.append(j)
 		
-		#从上右下左选一个方向
-		var id = 0;
-		for i in scene_can_go:
-			if i == 1:
-				choose_position_id -= 1
-			if choose_position_id == 0:
-				choose_position = id
-				scene_can_go[id] = 0
-				cnt -= 1
-				break
-			id += 1
-			
-		#上右下左衍生场景
+		if available_dirs.is_empty():
+			break
+		
+		var choose_position = available_dirs[randi_range(0, available_dirs.size()-1)]
+		scene_can_go[choose_position] = 0
+		
+		var new_pos = start_point + directions[choose_position]
+		map_arr.append(new_pos)
+		
+		# 更新父房间和子房间的连接关系
+		if !scene_to_scene.has(start_point):
+			scene_to_scene[start_point] = [0,0,0,0]
+		if !scene_to_scene.has(new_pos):
+			scene_to_scene[new_pos] = [0,0,0,0]
+		
 		match choose_position:
-			0:
-				var new_x: int = start_point[0]-1
-				var new_y: int = start_point[1]
-				var new_pos = Vector2i(new_x,new_y)
-				
-				if if_create(new_pos):
-					map_arr.append([new_x,new_y])
-				else:
-					map_types_ref.map_type_total_cnt += 1
-					continue
-				
-				scene_to_scene[new_start_point][0] = 1
-				if !scene_to_scene.has(new_pos):
-					scene_to_scene[new_pos] = [0,0,1,0]
-				else:
-					scene_to_scene[new_pos][2] = 1
-			1:
-				var new_x: int = start_point[0]
-				var new_y: int = start_point[1]+1
-				var new_pos = Vector2i(new_x,new_y)
-				
-				if if_create(new_pos):
-					map_arr.append([new_x,new_y])
-				else:
-					map_types_ref.map_type_total_cnt += 1
-					continue
-				
-				scene_to_scene[new_start_point][1] = 1
-				if !scene_to_scene.has(new_pos):
-					scene_to_scene[new_pos] = [0,0,0,1]
-				else:
-					scene_to_scene[new_pos][3] = 1
-			2:
-				var new_x: int = start_point[0]+1
-				var new_y: int = start_point[1]
-				var new_pos = Vector2i(new_x,new_y)
-				
-				if if_create(new_pos):
-					map_arr.append([new_x,new_y])
-				else:
-					map_types_ref.map_type_total_cnt += 1
-					continue
-				
-				scene_to_scene[new_start_point][2] = 1
-				if !scene_to_scene.has(new_pos):
-					scene_to_scene[new_pos] = [1,0,0,0]
-				else:
-					scene_to_scene[new_pos][0] = 1
-			3:
-				var new_x: int = start_point[0]
-				var new_y: int = start_point[1]-1
-				var new_pos = Vector2i(new_x,new_y)
-				
-				if if_create(new_pos):
-					map_arr.append([new_x,new_y])
-				else:
-					map_types_ref.map_type_total_cnt += 1
-					continue
-				
-				scene_to_scene[new_start_point][3] = 1
-				if !scene_to_scene.has(new_pos):
-					scene_to_scene[new_pos] = [0,1,0,0]
-				else:
-					scene_to_scene[new_pos][1] = 1
-			_:
-				print("如果你看到这个，就说明有错误在map_position_create脚本")
+			0: # 左
+				scene_to_scene[start_point][0] = 1
+				scene_to_scene[new_pos][2] = 1
+			1: # 下
+				scene_to_scene[start_point][1] = 1
+				scene_to_scene[new_pos][3] = 1
+			2: # 右
+				scene_to_scene[start_point][2] = 1
+				scene_to_scene[new_pos][0] = 1
+			3: # 上
+				scene_to_scene[start_point][3] = 1
+				scene_to_scene[new_pos][1] = 1
+	print( " arr：", map_arr)
 
-#能否创建这个点
-func if_create(the_position: Vector2i):
+func if_create(the_position: Vector2i) -> bool:
 	if the_position.x >= 0 && the_position.x <= map_len_x && the_position.y >= 0 && the_position.y <= map_len_y:
-		if map[the_position.x][the_position.y] == "void" && map_types_ref.get_map_count_now() > 0:
-			return true
+		if map[the_position.x][the_position.y] == "void" && !map_arr.has(the_position):
+			if map_types_ref.get_map_count_now() > 0 :
+				return true
 	return false
 
-#创建什么场景
-func create_what():
-	while true:
-		
-		if map_types_ref.map_type_now.size() == 0:
-			return "void"
-			
-		var randi_num = randi_range(1, map_types_ref.map_type_now.size())
-		var id = 1
-		
-		for now_name in map_types_ref.map_type_now.keys():
-			if id == randi_num:
-				if map_types_ref.map_type_now[now_name] != 0:
-					map_types_ref.map_type_now[now_name] -= 1
-					return now_name
-				else:
-					map_types_ref.map_type_now.erase(now_name)
-					break
-			else:
-				id += 1
-		
-func bfs_search(start_point: Array):
-	var new_search_arr: Array = []
+func create_what() -> String:
+	if map_types_ref.map_type_now.size() == 0:
+		return "void"
+	var keys = map_types_ref.map_type_now.keys()
+	var randi_num = randi_range(0, keys.size()-1)
+	var now_name = keys[randi_num]
+	if map_types_ref.map_type_now[now_name] > 0:
+		map_types_ref.map_type_now[now_name] -= 1
+		return now_name
+	else:
+		map_types_ref.map_type_now.erase(now_name)
+		return create_what()
+
+func bfs_search():
+	var new_search_arr: Array[Vector2i] = []
 	while !map_search_arr.is_empty():
-		var now_point = map_search_arr.pop_front()
-		var new_point_arr = scene_to_scene[Vector2i(now_point[0],now_point[1])]
+		var now_point: Vector2i = map_search_arr.pop_front()
+		var new_point_arr = scene_to_scene[now_point]
 		var id = 0
 		for num in new_point_arr:
 			if num == 1:
 				match id:
-					0:
-						new_search_arr.append([now_point[0]-1,now_point[1]])
-					1:
-						new_search_arr.append([now_point[0],now_point[1]+1])
-					2:
-						new_search_arr.append([now_point[0]+1,now_point[1]])
-					3:
-						new_search_arr.append([now_point[0],now_point[1]-1])
-					_:
-						print("如果你看到这个,说明map_position_create出错了")
+					0: new_search_arr.append(Vector2i(now_point.x-1, now_point.y))
+					1: new_search_arr.append(Vector2i(now_point.x, now_point.y+1))
+					2: new_search_arr.append(Vector2i(now_point.x+1, now_point.y))
+					3: new_search_arr.append(Vector2i(now_point.x, now_point.y-1))
 			id += 1
 	
 	for point in new_search_arr:
@@ -226,4 +171,4 @@ func bfs_search(start_point: Array):
 	if !map_search_arr.is_empty():
 		max_dis_from_home += 1
 		max_dis_from_home_point = map_search_arr.back()
-		bfs_search(max_dis_from_home_point)
+		bfs_search()
