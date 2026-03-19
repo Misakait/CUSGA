b4f4a12b0b6d44e5b6754189872270ee276480e2
huhu9 <huhu998s@outlook.com>
2026-03-19 22:33:25 +0800

    Revert "Revert "fix：完全修复地图数量缺少以及小地图显示错误的bug""
    This reverts commit 94b26dc7

--- scripts/map_scripts/map_little.gd
+++ scripts/map_scripts/map_little.gd
@@ -10,7 +10,7 @@ var offeset_bridge: float = 10
 
 var cnt: int = 0
 
-#这个是 位置-场景 的字典，类型为{Vector2i : Node2D}
+# 位置-场景 的字典 {Vector2i : Node2D}
 var map_scene: Dictionary
 
 var map: Array
@@ -18,12 +18,10 @@ var s2s: Dictionary
 
 
 func _ready() -> void:
-	
 	map = map_position_create.map
 	s2s = map_position_create.scene_to_scene
 	
 	build_little_map()
-	
 	change_this_cell_color(1,1)
 	
 	
@@ -34,42 +32,40 @@ func build_little_map():
 				build_this_cell(x,y)
 				build_this_bridge(x,y)
 
-#建立房间
-#注意：position的第一个参数是列，第二个参数是行，与x行y列相反
-func build_this_cell(y: int, x: int):
-	
+# 建立房间
+func build_this_cell(x: int, y: int):
 	var cell = load(cell_pic)
 	var cell_scene = cell.instantiate()
-	cell_scene.position = Vector2(x * offeset_cell, y * offeset_cell)
+	cell_scene.position = Vector2(y * offeset_cell, x * offeset_cell)
 	cell_scene.get_node("Label").text = str(cnt)
-	cnt+=1
+	cnt += 1
 	map_scene[Vector2i(x,y)] = cell_scene
 	add_child(cell_scene)
 
-#连接房间
-func build_this_bridge(y: int, x: int):
-	var bridge = load(bridge_pic)
-	var bridge_scene = bridge.instantiate()
-	var cell_position = Vector2(x * offeset_cell, y * offeset_cell)
+# 连接房间
+func build_this_bridge(x: int, y: int):
+	var cell_position = Vector2(y * offeset_cell, x * offeset_cell)
 	
-	for i in range(0,4):
-		
-		if s2s[Vector2i(y,x)][i] == 1:
+	if !s2s.has(Vector2i(x,y)):
+		return
+	
+	for i in range(4):
+		if s2s[Vector2i(x,y)][i] == 1:
+			var bridge = load(bridge_pic)
+			var bridge_scene = bridge.instantiate()
 			match i:
-				0:
-					bridge_scene.position = Vector2(cell_position.x , cell_position.y - offeset_bridge)
-				1:
-					bridge_scene.position = Vector2(cell_position.x + offeset_bridge , cell_position.y)
-				2:
-					bridge_scene.position = Vector2(cell_position.x , cell_position.y + offeset_bridge)
-				3:
-					bridge_scene.position = Vector2(cell_position.x - offeset_bridge , cell_position.y)
-				_:
-					print("如果你看到这个，就说明有错误发生在map_little这个脚本")
-		add_child(bridge_scene)
+				0: bridge_scene.position = Vector2(cell_position.x , cell_position.y - offeset_bridge) # 上
+				1: bridge_scene.position = Vector2(cell_position.x + offeset_bridge , cell_position.y) # 右
+				2: bridge_scene.position = Vector2(cell_position.x , cell_position.y + offeset_bridge) # 下
+				3: bridge_scene.position = Vector2(cell_position.x - offeset_bridge , cell_position.y) # 左
+			add_child(bridge_scene)
 
-func change_this_cell_color(y: int,x :int):
-	map_scene[Vector2i(x,y)].get_node("Cell").self_modulate = Color.RED
-	
-func return_this_cell_color(y:int ,x: int):
-	map_scene[Vector2i(x,y)].get_node("Cell").self_modulate = Color.WHITE
+# 改变场景颜色
+func change_this_cell_color(x: int, y: int):
+	if map_scene.has(Vector2i(x,y)):
+		map_scene[Vector2i(x,y)].get_node("Cell").self_modulate = Color.RED
+
+# 恢复场景颜色
+func return_this_cell_color(x: int, y: int):
+	if map_scene.has(Vector2i(x,y)):
+		map_scene[Vector2i(x,y)].get_node("Cell").self_modulate = Color.WHITE
