e9344692d8a361d21fcfc1d5053d19334d25926c
misakait <cjl2597588144@gmail.com>
2026-03-20 11:09:10 +0800

    feat： 新增玩家背包常量

--- core/constants/TagConsts.cs
+++ core/constants/TagConsts.cs
@@ -4,8 +4,7 @@ namespace CUSGA.core.constants;
 
 public static class TagConsts
 {
-    // 以下是示例词条
-    public static readonly StringName BonusWoodDrop = new("BonusWoodDrop");
+    public static readonly StringName SystemInventory = new("SystemInventory");
     public static readonly StringName WoodDamageUp = new("WoodDamageUp");
     public static readonly StringName HealAfterAction = new("HealAfterAction");
 
