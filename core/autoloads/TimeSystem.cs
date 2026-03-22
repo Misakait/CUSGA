ba33a79df7288f5488ab83058df5d9c748ad9daf
misakait <cjl2597588144@gmail.com>
2026-03-22 16:33:22 +0800

    feat: 将实践系统的是否为夜晚改为可公开读取

--- core/autoloads/TimeSystem.cs
+++ core/autoloads/TimeSystem.cs
@@ -6,51 +6,52 @@ namespace CUSGA.core.autoloads;
 public partial class TimeSystem : Node
 {
 
-	// 昼夜更替广播
-	[Signal] public delegate void DayNightToggledEventHandler(bool isNight);
+    // 昼夜更替广播
+    [Signal] public delegate void DayNightToggledEventHandler(bool isNight);
 
-	// 天数增加广播
-	[Signal] public delegate void DayPassedEventHandler(int currentDay);
+    // 天数增加广播
+    [Signal] public delegate void DayPassedEventHandler(int currentDay);
 
-	// 触发天赋选择界面广播
-	[Signal] public delegate void TalentSelectionTriggeredEventHandler();
+    // 触发天赋选择界面广播
+    [Signal] public delegate void TalentSelectionTriggeredEventHandler();
 
-	private int _totalTimePassed = 0; // 记录开局以来的总时间流逝
-	private int _currentDay = 1;
-	private bool _isNight = false;
+    private int _totalTimePassed = 0; // 记录开局以来的总时间流逝
+    private int _currentDay = 1;
 
-	public void PassTime(int amount)
-	{
-		int previousTime = _totalTimePassed;
-		_totalTimePassed += amount;
+    public bool IsNight { get; private set; } = false;
 
-		GD.Print($"时间流逝了 {amount} 点，当前总时间：{_totalTimePassed}");
+    public void PassTime(int amount)
+    {
+        int previousTime = _totalTimePassed;
+        _totalTimePassed += amount;
 
-		CheckTimeTransitions(previousTime, _totalTimePassed);
-	}
+        GD.Print($"时间流逝了 {amount} 点，当前总时间：{_totalTimePassed}");
 
-	private void CheckTimeTransitions(int oldPoints, int newPoints)
-	{
-		int oldPhase = oldPoints / 100;
-		int newPhase = newPoints / 100;
+        CheckTimeTransitions(previousTime, _totalTimePassed);
+    }
 
-		if (newPhase > oldPhase)
-		{
-			_isNight = !_isNight; // 昼夜翻转
-			EmitSignal(SignalName.DayNightToggled, _isNight);
+    private void CheckTimeTransitions(int oldPoints, int newPoints)
+    {
+        int oldPhase = oldPoints / 100;
+        int newPhase = newPoints / 100;
 
-			// 如果阶段是偶数，说明新的一天开始了
-			if (newPhase % 2 == 0)
-			{
-				_currentDay++;
-				EmitSignal(SignalName.DayPassed, _currentDay);
+        if (newPhase > oldPhase)
+        {
+            IsNight = !IsNight; // 昼夜翻转
+            EmitSignal(SignalName.DayNightToggled, IsNight);
 
-				// 触发天赋选择
-				if (_currentDay % 7 == 0)
-				{
-					EmitSignal(SignalName.TalentSelectionTriggered);
-				}
-			}
-		}
-	}
+            // 如果阶段是偶数，说明新的一天开始了
+            if (newPhase % 2 == 0)
+            {
+                _currentDay++;
+                EmitSignal(SignalName.DayPassed, _currentDay);
+
+                // 触发天赋选择
+                if (_currentDay % 7 == 0)
+                {
+                    EmitSignal(SignalName.TalentSelectionTriggered);
+                }
+            }
+        }
+    }
 }
