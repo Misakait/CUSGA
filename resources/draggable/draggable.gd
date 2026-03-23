f35e832a06d3d135f5082e650d8648eb0b81b3a4
huhu9 <huhu998s@outlook.com>
2026-03-23 21:15:03 +0800

    feat：增加了drag_func代码，用来拖动sprite2d，这也许是解耦了？

--- resources/draggable/draggable.gd
+++ resources/draggable/draggable.gd
@@ -1,6 +1,8 @@
 extends Resource
 class_name Draggable
 
+@export var drag_speed: int = 1
+
 var dragging := false
 var drag_offset := Vector2.ZERO
 
@@ -8,9 +10,9 @@ func handle_event(event: InputEvent, target_node) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			dragging = true
-			drag_offset = event.position
+			drag_offset = event.position - target_node.position
 		else:
 			dragging = false
-
+	
 	if event is InputEventMouseMotion and dragging:
-		target_node.position += event.relative
+		target_node.position = event.position - drag_offset
--- scripts/normal_compoment/drag_func.gd
+++ scripts/normal_compoment/drag_func.gd
@@ -1,9 +1,48 @@
-extends TextureRect
+extends Node2D
 
 @export var draggable: Draggable
 
-func _gui_input(event):
-	if draggable:
-		draggable.handle_event(event, self)
-	else:
-		print("你没有设置draggable")
+const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
+
+var card
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			card = raycast_check_for_card()
+			if card:
+				start_drag(card)
+		else:
+			finish_drag()
+	if card:
+		draggable.handle_event(event, card)
+		
+func start_drag(card):
+	pass
+
+func finish_drag():
+	card = null
+
+#光线投射，检查并获取鼠标下的卡牌
+func raycast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return get_card_with_highest_z_index(result)
+	return null
+	
+#获取传入卡牌中z最高的牌
+func get_card_with_highest_z_index(cards):
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	for i in range(1, cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
