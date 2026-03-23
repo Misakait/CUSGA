01337e5c7d33ee1da5d21c27887652a64edd734a
misakait <cjl2597588144@gmail.com>
2026-03-23 22:12:25 +0800

    Add GDSignals signal constants
    Introduce GDSignals static class exposing StringName constants for
    on_inventory_toggled and on_player_acquired_talent signals

---
+++ core/constants/GDSignals.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+namespace CUSGA.core.constants;
+
+public static class GDSignals
+{
+    public static readonly StringName OnInventoryToggled = new("on_inventory_toggled");
+    public static readonly StringName OnPlayerAcquiredTalent = new("on_player_acquired_talent");
+}
