fa90e3e6a03cd8308f0a73480f9353fb6051a497
misakait <cjl2597588144@gmail.com>
2026-03-24 23:02:19 +0800

    Use Node for DamagePayload source and add target
    Add IsExtraDamage boolean flag (default false) to mark extra
    damage instances

--- core/combat/DamagePayload.cs
+++ core/combat/DamagePayload.cs
@@ -13,8 +13,10 @@ public enum DamageType
 
 public class DamagePayload
 {
-    public Player Source { get; set; }
+    public Node Source { get; set; }
+    public Node Target { get; set; }
     public DamageType Type { get; set; }
     public int Damage { get; set; }
     public ElementType Element { get; set; }
+    public bool IsExtraDamage { get; set; } = false;
 }
