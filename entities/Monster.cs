f61542e9d3e6f97024f38a09395cc5b40a4f32a4
misakait <cjl2597588144@gmail.com>
2026-03-24 22:59:18 +0800

    Remove ReceiveCardAttack method

--- entities/Monster.cs
+++ entities/Monster.cs
@@ -11,7 +11,7 @@ namespace CUSGA.entities;
 public partial class Monster : Node2D
 {
     [Export]
-    public MonsterData BaseData { get;set; }
+    public MonsterData BaseData { get; set; }
 
     public HealthComponent Health { get; private set; }
     public AttributeComponent Attributes { get; private set; }
@@ -53,60 +53,5 @@ public partial class Monster : Node2D
         {
             BehaviorTree.AddChild(behaviorTree);
         }
-    }
-
-    public void ReceiveCardAttack(DamagePayload payload)
-    {
-        var attackerStats = payload.Source.GetNode<AttributeComponent>("AttributeComponent");
-        var defenderStats = GetNode<AttributeComponent>("AttributeComponent");
-
-        float calculatedDamage = payload.Damage;
-
-        // 攻击方增伤
-        if (payload.Type == DamageType.Physical)
-        {
-            float flatPhysPower = attackerStats.GetAttribute(AttributeType.PhysAtk)?.Value ?? 0f;
-            calculatedDamage += flatPhysPower;
-
-            float physBoostPct = attackerStats.GetAttribute(AttributeType.PhysDamageBoost)?.Value ?? 0f;
-            calculatedDamage *= (1f + physBoostPct);
-        }
-        else if (payload.Type == DamageType.Magic)
-        {
-            float flatMagPower = attackerStats.GetAttribute(AttributeType.MagPower)?.Value ?? 0f;
-            calculatedDamage += flatMagPower;
-
-            float magicBoostPct = attackerStats.GetAttribute(AttributeType.MagicDamageBoost)?.Value ?? 0f;
-            calculatedDamage *= (1f + magicBoostPct);
-        }
-
-        // 元素反应区
-        float elementMult = ElementalSystem.CalculateMultiplier(payload.Element, BaseData.ElementalProperty);
-        calculatedDamage *= elementMult;
-
-        // 防御抗性减伤区
-        if (payload.Type != DamageType.Real)
-        {
-            float targetDefense = 0f;
-
-            if (payload.Type == DamageType.Physical)
-            {
-                targetDefense = defenderStats.GetAttribute(AttributeType.PhysDef)?.Value ?? 0f;
-            }
-            else if (payload.Type == DamageType.Magic)
-            {
-                targetDefense = defenderStats.GetAttribute(AttributeType.MagResist)?.Value ?? 0f;
-            }
-
-            // 最终护甲
-            float finalDefense = Mathf.Max(0, targetDefense);
-
-            float defenseMultiplier = 100f / (100f + finalDefense);
-
-            calculatedDamage *= defenseMultiplier;
-        }
-
-        int finalDamageInt = Mathf.RoundToInt(calculatedDamage);
-        GetNode<HealthComponent>("HealthComponent").TakeDamage(finalDamageInt, payload.Element);
     }
 }
