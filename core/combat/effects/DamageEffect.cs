71b19b43aedb94813cf2d65ef8b4e61e5cd747e9
LIsBam <2310773349@qq.com>
2026-04-09 22:47:04 +0800

    feat：为damageeffect新增元素属性。

--- core/combat/effects/DamageEffect.cs
+++ core/combat/effects/DamageEffect.cs
@@ -1,4 +1,5 @@
 using CUSGA.entities.components;
+using CUSGA.core.constants;
 using Godot;
 namespace CUSGA.core.combat.effects;
 
@@ -7,10 +8,11 @@ public partial class DamageEffect : CardEffect
 {
     [Export] public int BaseDamage { get; set; } = 10;
     [Export] public DamageType Type { get; set; } = DamageType.Physical;
+    [Export] public ElementType Element { get; set; } = ElementType.None;
 
     public override void Execute(Node source, Node target)
     {
-        var payload = new DamagePayload { Source = source, Damage = BaseDamage, Type = Type };
+        var payload = new DamagePayload { Source = source, Target = target, Damage = BaseDamage, Type = Type, Element = Element };
         // 受伤的代码我还没完全弄好，只是示例，不过以后应该差不多这样
         target.GetNodeOrNull<DamageReceiverComponent>("DamageReceiverComponent")?.ReceiveDamage(payload);
     }
