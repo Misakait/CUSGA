cba458634bffe7a44a85bd424ec5030a7131fdd9
misakait <cjl2597588144@gmail.com>
2026-04-20 15:35:49 +0800

    Use AttributeComponent.AddPermanentBonus

--- resources/talents/AttributeTalentEffect.cs
+++ resources/talents/AttributeTalentEffect.cs
@@ -7,21 +7,17 @@ namespace CUSGA.resources.talents;
 [GlobalClass]
 public partial class AttributeTalentEffect : TalentEffect
 {
-	[Export] public AttributeType TargetAttribute { get; set; }
-	[Export] public float BonusValue { get; set; }
+    [Export] public AttributeType TargetAttribute { get; set; }
+    [Export] public float BonusValue { get; set; }
 
-	public override void Apply(Player targetPlayer)
-	{
-		var attrComp = targetPlayer.GetNodeOrNull<AttributeComponent>("AttributeComponent");
+    public override void Apply(Player targetPlayer)
+    {
+        var attrComp = targetPlayer.GetNodeOrNull<AttributeComponent>("AttributeComponent");
 
-		if (attrComp != null)
-		{
-			var attribute = attrComp.GetAttribute(TargetAttribute);
-			if (attribute != null)
-			{
-				attribute.AddBonus(BonusValue);
-				GD.Print($"天赋生效：{TargetAttribute} 永久增加了 {BonusValue}！");
-			}
-		}
-	}
+        if (attrComp != null)
+        {
+            attrComp.AddPermanentBonus(TargetAttribute, BonusValue);
+            GD.Print($"天赋生效：{TargetAttribute} 永久增加了 {BonusValue}！");
+        }
+    }
 }
