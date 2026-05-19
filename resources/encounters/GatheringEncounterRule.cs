0abe0f39bf512189b1232f7a132bd69ead5cbfd5
misakait <cjl2597588144@gmail.com>
2026-05-19 22:16:51 +0800

    Support multiple monsters in gathering encounters

--- resources/encounters/GatheringEncounterRule.cs
+++ resources/encounters/GatheringEncounterRule.cs
@@ -1,20 +1,21 @@
 using CUSGA.resources.monsters;
 using Godot;
+using Godot.Collections;
 
 namespace CUSGA.resources.encounters;
 
 [GlobalClass]
 public partial class GatheringEncounterRule : Resource
 {
-	// 触发条件：砍的是什么标签的资源
-	[Export] public StringName TriggerTag { get; set; }
+    // 触发条件：砍的是什么标签的资源
+    [Export] public StringName TriggerTag { get; set; }
 
-	// 生成的怪物
-	[Export] public MonsterData MonsterToSpawn { get; set; }
+    // 生成的怪物
+    [Export] public Array<MonsterData> MonsterToSpawn { get; set; }
 
-	//生成时的 UI 提示语
-	[Export] public string SpawnMessage { get; set; } = "糟糕！采集物变成了怪物！";
+    //生成时的 UI 提示语
+    [Export] public string SpawnMessage { get; set; } = "糟糕！采集物变成了怪物！";
 
-	// 生成概率的附加权重系数
-	[Export] public float ExtraChanceMultiplier { get; set; } = 1.0f;
+    // 生成概率的附加权重系数
+    [Export] public float ExtraChanceMultiplier { get; set; } = 1.0f;
 }
