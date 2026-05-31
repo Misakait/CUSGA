cfc515a00387ebaeb8356f1bc0f57142bb49caba
misakait <cjl2597588144@gmail.com>
2026-05-31 21:48:39 +0800

    refactor: migrate MaxHealth to StartingStats and init default

--- resources/monster/MonsterData.cs
+++ resources/monster/MonsterData.cs
@@ -13,7 +13,7 @@ public enum MonsterFaction { Hostile, PlayerSummon, Neutral }
 public partial class MonsterData : Resource
 {
     [Export] public string MonsterName { get; set; } = "未知怪物";
-    [Export] public StartingStats InitialAttributes { get; set; }
+    [Export] public StartingStats InitialAttributes { get; set; } = new();
     // 怪物默认的五行属性
     [Export] public ElementType ElementalProperty { get; set; } = ElementType.None;
     // 怪物的外观预制体
@@ -21,7 +21,6 @@ public partial class MonsterData : Resource
     [Export] public LootTable LootTable { get; set; }
     [Export] public PackedScene BehaviorTreeScene { get; set; }
     [Export] public MonsterFaction Faction { get; set; }
-    [Export] public int MaxHealth { get; set; }
 
     // 怪物只配置战斗技能集合，玩家卡牌资源只在玩家牌组和 UI 表现边界使用
     [Export] public MonsterSkillSetData SkillSet { get; set; }
